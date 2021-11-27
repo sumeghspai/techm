0d5f8f4bf3b1baa8a9240610709d747651aec0e2
Vishnu <vishnupothia@gmail.com>
2021-11-27 19:39:51 +0530

    basic proj

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded());
 app.use(express.static("assets"));
 
+var Data = []
+
 app.get('/',function(req,res){
     res.render('index');
+});
+
+app.get('/form',function(req,res){
+    res.render('form');
+});
+
+app.post('/submit',function(req,res){
+    Data.push(req.body);
+    console.log(req.body);
+    res.redirect('/')
 })
 
 app.listen(port,'0.0.0.0',function(err){
